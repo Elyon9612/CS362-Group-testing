8092e2fbff6d201e2b93ed882056e876b349b806
HeySiri1998 <34022268+Sing1998@users.noreply.github.com>
2020-05-29 20:13:24 -0700

    Test4 created for Hex
    '0xAD4' 
    expect result:  2772

--- tests.py
+++ tests.py
@@ -15,6 +15,10 @@ class TestCase(unittest.TestCase):
     def test3(self):
         test_num = '.45'
         self.assertEqual(0.45, conv_num(test_num))
+
+    def test4(self):
+        test_num = '0xAD4'
+        self.assertEqual(2772, conv_num(test_num))
        
     
 if __name__ == "__main__":
