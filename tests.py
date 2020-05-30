800dfb622583ea67e18f3ccc1fbb9597e145e695
HeySiri1998 <34022268+Sing1998@users.noreply.github.com>
2020-05-29 17:48:14 -0700

    Test 2 added
    float number: 123.45
    expect: True

--- tests.py
+++ tests.py
@@ -7,6 +7,10 @@ class TestCase(unittest.TestCase):
     def test1(self):
         test_num = '12345'
         self.assertTrue(conv_num(test_num), 12345)
-
+   
+    def test2(self):
+        test_num = '123.45'
+        self.assertTrue(conv_num(test_num), 123.45)
+       
 if __name__ == "__main__":
     unittest.main()
